731e847b618a090c0c9e0c29751ec5f9268d37cb
SAMARTH <SAMARTH@LAPTOP-6HPBE1TN>
2023-01-14 17:17:11 +0530

    commit1

---
+++ JavaPrograms/src/assignments/StringExample.java
@@ -0,0 +1,30 @@
+package assignments;
+
+import java.util.Scanner;
+
+public class StringExample {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		// input string 1
+		System.out.println("Enter Frist String "); 
+		String str1= sc.next();
+		
+		// input string 2
+		System.out.println("Enter Second String ");
+		String str2= sc.next();
+		
+		// compare 2 string 
+		if(str1.equals(str2))
+		{
+			System.out.println("Both String are Equal....");
+		}
+		else
+		{
+			System.out.println("String are not equal....");
+		}
+	}
+
+}
